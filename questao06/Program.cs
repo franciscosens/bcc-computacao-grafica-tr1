46ac8e63e843a2c53f1f12ca62244d4f4e305a35
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-08-29 20:19:52 -0300

    Criado os projetos para resolver as questões

---
+++ questao06/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenTK;
+
+namespace questao06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow window = new GameWindow(600, 600);
+            window.Run(1.0 / 60.0);
+        }
+    }
+}
