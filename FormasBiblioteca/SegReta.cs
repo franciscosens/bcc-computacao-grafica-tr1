35a5d4402306cfade929094eaa290953aaee645b
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-09-20 14:37:14 -0300

    Desenvolvimento da questão 05

--- FormasBiblioteca/SegReta.cs
+++ FormasBiblioteca/SegReta.cs
@@ -1,22 +1,120 @@
 using System.Drawing;
 using CG_Biblioteca;
 using OpenTK.Graphics.OpenGL;
+using System;
 
 namespace FormasBiblioteca
 {
     public class SegReta : ObjetoAramado
     {
+        private int _angulo;
+        private string metodo;
+        private double distancia = 100;
+        private Matematica matematica;
 
         public SegReta(string rotulo, Ponto4D ponto, Ponto4D ponto2) : base(rotulo)
         {
+            matematica = new Matematica();
             base.PontosAdicionar(ponto);
             base.PontosAdicionar(ponto2);
         }
-        
-        public SegReta(string rotulo, Ponto4D ponto, Ponto4D ponto2, Color cor, float primitivaTamanho) : base(rotulo, cor, primitivaTamanho)
+
+        public SegReta(string rotulo, Ponto4D ponto, Ponto4D ponto2, Color cor, float primitivaTamanho, int angulo = 0) : base(rotulo, cor, primitivaTamanho)
         {
+            matematica = new Matematica();
             base.PontosAdicionar(ponto);
             base.PontosAdicionar(ponto2);
+            this._angulo = angulo;
         }
+
+        public string GetRotulo()
+        {
+            return base.rotulo;
+        }
+
+        public void IncrementarAngulo()
+        {
+            metodo = "IncrementarAngulo";
+            this._angulo = this._angulo > 360 ? 0 : this._angulo + 1;
+            // if (this._angulo > 360)this._angulo = 0;
+            // else this._angulo++;
+            AtualizarPonto();
+        }
+
+        public void DecrementarAngulo()
+        {
+            metodo = "DecrementarAngulo";
+            this._angulo = this._angulo <= 0 ? 360 : this._angulo - 1;
+            // if (this._angulo <= 0) this._angulo = 360;
+            // else this._angulo--;
+            AtualizarPonto();
+        }
+
+        private void AtualizarPonto(int inc = 0)
+        {
+            Ponto4D ponto = base.pontosLista[0];
+            Ponto4D ponto2 = base.pontosLista[1];
+
+            double x = ponto2.X - ponto.X;
+            double y = ponto2.Y - ponto.Y;
+            distancia = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+            Ponto4D pontoNovo = matematica.GerarPtosCirculo(_angulo, distancia + inc);
+            DefinirPonto(ponto, pontoNovo);
+        }
+
+        public void Aumentar()
+        {
+            metodo = "Aumentar";
+            AtualizarPonto(1);
+        }
+
+        public void Diminuir()
+        {
+            metodo = "Diminuir";
+            AtualizarPonto(-1);
+        }
+
+        public void Avancar()
+        {
+            Ponto4D ponto = base.pontosLista[0];
+            Ponto4D ponto2 = base.pontosLista[1];
+
+            Ponto4D pontoNovo = new Ponto4D();
+            pontoNovo.X = ponto.X + 1;
+            pontoNovo.Y = ponto.Y;
+
+            Ponto4D pontoNovo2 = new Ponto4D();
+            pontoNovo2.X = ponto2.X + 1;
+            pontoNovo2.Y = ponto2.Y;
+
+            metodo = "Avançar";
+            DefinirPonto(pontoNovo, pontoNovo2);
+        }
+
+        public void Retornar()
+        {
+            metodo = "Retornar";
+            Ponto4D ponto = base.pontosLista[0];
+            ponto.X = ponto.X - 1;
+            Ponto4D ponto2 = base.pontosLista[1];
+            ponto2.X = ponto2.X - 1;
+            DefinirPonto(ponto, ponto2);
+        }
+
+        public void DefinirPonto(Ponto4D ponto, Ponto4D ponto2)
+        {
+            base.PontosRemoverTodos();
+            base.PontosAdicionar(ponto);
+            base.PontosAdicionar(ponto2);
+            Console.Write(metodo);
+            for (int i = 30; i >= metodo.Length; i--)
+            {
+                Console.Write(" ");
+            }
+
+            System.Console.WriteLine($"\tPonto: {ponto}\t\tPonto2: {ponto2}\t\tAngulo: {_angulo}");
+        }
+
     }
 }
---
+++ questao05/Mundo.cs
@@ -0,0 +1,196 @@
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using OpenTK.Input;
+using CG_Biblioteca;
+using FormasBiblioteca;
+using System.Collections.Generic;
+
+namespace questao05
+{
+
+    public class Mundo : GameWindow
+    {
+        private SegReta _segReta;
+        public const double Distancia = 5, DistanciaAngulo = 1;
+        public static Mundo instance = null;
+
+        private Matematica matematica;
+        private double raio = 100;
+        private int angulo = 45;
+
+        public Mundo(int width, int height) : base(width, height)
+        {
+            matematica = new Matematica();
+        }
+
+        public static Mundo getInstance()
+        {
+            if (instance == null)
+                instance = new Mundo(600, 600);
+            return instance;
+        }
+
+        private Camera camera = new Camera();
+        protected List<Objeto> objetosLista = new List<Objeto>();
+        private bool moverPto = false;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            // GerarLinhas();
+
+            var segmentoRetaA = new SegReta("A", new Ponto4D(0, 0, 0), new Ponto4D(200, 0, 0), Color.Red, 5);
+            var segmentoRetaB = new SegReta("B", new Ponto4D(0, 0, 0), new Ponto4D(0, 200, 0), Color.Green, 5);
+            _segReta = new SegReta("C", new Ponto4D(0, 0, 0), matematica.GerarPtosCirculo(angulo, raio), Color.Black, 5, angulo);
+
+            objetosLista.Add(segmentoRetaA);
+            objetosLista.Add(segmentoRetaB);
+            objetosLista.Add(_segReta);
+            GL.ClearColor(Color.Gray);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            //GL.Ortho(camera.xmin, camera.xmax, camera.ymin, camera.ymax, camera.zmin, camera.zmax);
+            GL.Ortho(-300, 300, -300, 300, -1, 1);
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            for (var i = 0; i < objetosLista.Count; i++)
+            {
+                objetosLista[i].Desenhar();
+            }
+
+            this.SwapBuffers();
+        }
+
+        private void GerarLinhas()
+        {
+            GL.LineWidth(5);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex2(0, 0); GL.Vertex2(200, 0);
+            GL.Color3(Color.Green);
+            GL.Vertex2(0, 0); GL.Vertex2(0, 200);
+            GL.End();
+        }
+
+
+        protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Exit();
+            else
+            if (e.Key == Key.W || e.Key == Key.Q || e.Key == Key.Z || e.Key == Key.X || e.Key == Key.A || e.Key == Key.S)
+            {
+                for (var i = 0; i < objetosLista.Count; i++)
+                {
+                    if (objetosLista[i] is SegReta)
+                    {
+                        var reta = (SegReta)objetosLista[i];
+                        if (reta.GetRotulo() == "C")
+                        {
+                            switch (e.Key)
+                            {
+                                case Key.Q:
+                                    // AlterarPosicao(Distancia * -1, ref reta);
+                                    _segReta.Retornar();
+                                    break;
+                                case Key.W:
+                                    // AlterarPosicao(Distancia, ref reta);
+                                    _segReta.Avancar();
+                                    break;
+                                case Key.Z:
+                                    // AlterarZoom(Distancia * -1, ref reta);
+                                    _segReta.Diminuir();
+                                    break;
+                                case Key.X:
+                                    // AlterarZoom(Distancia, ref reta);
+                                    _segReta.Aumentar();
+                                    break;
+                                case Key.A:
+                                    // Rotacionar(DistanciaAngulo * -1, ref reta);
+                                    _segReta.DecrementarAngulo();
+                                    break;
+                                case Key.S:
+                                    // Rotacionar(DistanciaAngulo, ref reta);
+                                    _segReta.IncrementarAngulo();
+                                    break;
+                            }
+                        }
+                    }
+                    //objetosLista[i].PontosExibirObjeto();
+                }
+            }
+            else
+            if (e.Key == Key.M)
+            {
+                moverPto = !moverPto;
+            }
+        }
+
+        // private void Rotacionar(double adicional, ref SegReta reta)
+        // {
+        //     System.Console.WriteLine(angulo);
+        //     if (angulo + adicional >= 360)
+        //     {
+        //         angulo = 0;
+        //     }
+        //     else if (angulo + adicional <= 0)
+        //     {
+        //         angulo = 360;
+        //     }
+        //     else
+        //     {
+        //         angulo += adicional;
+        //     }
+
+
+        //     var pontoNovo = matematica.GerarPtosCirculo(angulo, raio); ;
+        //     reta.DefinirPonto(reta.ponto, pontoNovo);
+        //     // reta.ponto2.X = pontoNovo.X;
+        //     // reta.ponto2.Y = pontoNovo.Y;
+        // }
+
+        // private void AlterarPosicao(double adicional, ref SegReta reta)
+        // {
+        //     // reta.ponto.X = reta.ponto.X + adicional;
+        //     // reta.ponto2.X = reta.ponto2.X + adicional;
+        //     var ponto1 = reta.ponto;
+        //     ponto1.X = ponto1.X + adicional;
+        //     var ponto2 = reta.ponto2;
+        //     ponto2.X = ponto2.X + adicional;
+        //     reta.DefinirPonto(ponto1, ponto2);
+        // }
+
+        // private void AlterarZoom(double adicional, ref SegReta reta)
+        // {
+
+        //     raio += adicional;
+        //     var pontoNovo = matematica.GerarPtosCirculo(angulo, raio);
+        //     // reta.ponto2.X = pontoNovo.X;
+        //     // reta.ponto2.Y = pontoNovo.Y;
+        //     reta.DefinirPonto(reta.ponto, pontoNovo);
+        // }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            if (moverPto)
+            {
+                //retanguloB.MoverPtoSupDir(new Ponto4D(e.Position.X, 600 - e.Position.Y, 0));
+            }
+        }
+    }
+}
