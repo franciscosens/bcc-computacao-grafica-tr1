b4088604418cbc6501790a6c297924df45333fdd
bvigentas <brunovigentas@gmail.com>
2019-09-20 18:53:05 -0300

    Changed color

--- FormasBiblioteca/PontosExtremos.cs
+++ FormasBiblioteca/PontosExtremos.cs
@@ -13,23 +13,27 @@ namespace FormasBiblioteca
 
         private PrimitiveType primitiva;
 
-        public PontosExtremos(string rotulo, Color cor, int primitivaTamanho, PrimitiveType primitivaTipo, List<Ponto4D> pontos) 
-            : base(rotulo, cor, primitivaTamanho, primitivaTipo) {
+        public PontosExtremos(string rotulo, PrimitiveType primitivaTipo, List<Ponto4D> pontos): base(rotulo) {
+            Primitiva = primitivaTipo;
             this.pontos = pontos;
-            this.primitiva = primitivaTipo;
         }
 
-        public void ligarPontos() {
-            GL.Begin(this.primitiva);
+        protected override void DesenharAramado() {
+            GL.Begin(Primitiva);
             GL.Color3(Color.Cyan);
             GL.Vertex3(pontos[0].X, pontos[0].Y, 0);
-            GL.Color3(Color.Pink);
+            GL.Color3(Color.Magenta);
             GL.Vertex3(pontos[1].X, pontos[1].Y, 0);
             GL.Color3(Color.Yellow);
             GL.Vertex3(pontos[2].X, pontos[2].Y, 0);
             GL.Color3(Color.Black);
             GL.Vertex3(pontos[3].X, pontos[3].Y, 0);
             GL.End();
+        }
+
+        public PrimitiveType Primitiva{
+            get => primitiva;
+            set => primitiva = value;
         }
     }
 }
